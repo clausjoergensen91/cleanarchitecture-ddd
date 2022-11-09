7bb58e025fdcd849783bc28c5fe7291adf00e3d6
Claus Jørgensen <clausjoergensen91@gmail.com>
2022-11-09 12:33:38 +0100

    Added "SmartEnum" functionality example

---
+++ Console.FunctionalityTest/Program.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Enums.SmartEnum;
+
+var creditCardStandard = CreditCard.FromName("Standard");
+var creditCardPremium = CreditCard.FromName("Premium");
+var creditCardPlatinum = CreditCard.FromName("Platinum");
+
+Console.WriteLine($"Discount for {creditCardStandard} is {creditCardStandard.Discount:P}");
+Console.WriteLine($"Discount for {creditCardPremium} is {creditCardPremium.Discount:P}");
+Console.WriteLine($"Discount for {creditCardPlatinum} is {creditCardPlatinum.Discount:P}");
+
+Console.ReadKey();
